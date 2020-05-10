2049c739ffabb55d533243c21804528b83e1d6a3
Lidia Tkach <tkach.lidia61@gmail.com>
2020-05-10 17:03:54 +0300

    RegEx homework completed

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,15 +1,18 @@
-/**
- * Regular expressions.
- */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegExp {
 
-  /**
-   * Get IP address from text.
-   * @param text text to parse
-   * @return String with IP address
-   */
-  public static String getIpAddress(String text) {
-    // TODO fill in code here for extracting IP address using Regular Expression
-    return "";
-  }
+    public static String getIpAddress(String text) {
+
+        String ipAddress = "((25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9]))";
+
+        Pattern pattern = Pattern.compile(ipAddress);
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            return matcher.group();
+        } else {
+            return "";
+        }
+    }
 }
